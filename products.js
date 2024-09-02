246d0756db2674e0373f6c749f6bbed8db7a8d65
Shadow <qaz889065@gmail.com>
2024-09-02 14:40:43 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,31 @@
+const products = [
+  {
+    id: 1,
+    Websitelink: "https://dashing-nasturtium-fdb1ab.netlify.app",
+    description:
+      " متجر لعرض المنتجات والطلب اونلاين ومتجاوب مع جميع الشاشات",
+    imageLink: "https://res.cloudinary.com/dtomtlfcs/image/upload/v1725139919/Captureasdwd_hm9n5o.png",
+  },
+
+  {
+    id: 2,
+    Websitelink: "https://material-ui-eta-indol.vercel.app/",
+    description:
+      "  وجه لمنصه للتواصل حول العالم ومتجاوب مع جميع الشاشات",
+    imageLink: "https://res.cloudinary.com/dtomtlfcs/image/upload/v1725139921/asdwdd_fhr2wz.png",
+  },
+  {
+    id: 3,
+    Websitelink: "https://firebase-fire-store.vercel.app/",
+    description:
+      "موقع لارسال الداتا وعرضها في الموقع ومتعدد الغات ويمكن تحويله الي Appliction",
+    imageLink: "https://res.cloudinary.com/dtomtlfcs/image/upload/v1725139907/asdcsa_lpimoi.png",
+  }, {
+    id: 4,
+    Websitelink: "https://firebase-authentication-six-chi.vercel.app/",
+    description:
+      "نظام تسجيل الدخول والخروج وححفظ المعلومات ",
+    imageLink: "https://res.cloudinary.com/dtomtlfcs/image/upload/v1725139907/adqwdqa_cpxzbp.png",
+  },
+];
+module.exports = products;
